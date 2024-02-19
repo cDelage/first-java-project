5c08d9f592e77218d5bf810e8c901bbf5b1cd71f
cDelage <delage.corentinpro@gmail.com>
2024-02-19 10:08:06 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        var toto = "Hello boy";
+        System.out.println(toto);
+
+    }
+}
